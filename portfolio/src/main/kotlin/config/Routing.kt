92419a7eb136356884122202efb8fa71e389fca8
pema4 <petelin.maksim@live.com>
2023-05-30 21:43:47 +0300

    rnbo test

--- portfolio/src/main/kotlin/config/Routing.kt
+++ portfolio/src/main/kotlin/config/Routing.kt
@@ -1,6 +1,8 @@
 package lerpmusic.portfolio.config
 
-import io.ktor.server.application.Application
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.partialcontent.*
 import io.ktor.server.routing.routing
 import lerpmusic.portfolio.route.masterPortfolioRoute
 import lerpmusic.portfolio.route.staticRoute
@@ -9,5 +11,9 @@ fun Application.configureRouting() {
     routing {
         masterPortfolioRoute()
         staticRoute()
+        static("/rnbo") {
+            install(PartialContent)
+            resources("rnbo")
+        }
     }
 }
