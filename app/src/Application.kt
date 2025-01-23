375bd1dd8b4fe098e86f8f3da84f9b7979a6c3c4
pema4 <petelin.maksim@live.com>
2025-01-24 00:59:15 +0300

    Add lerpmusic.ru/index.html

--- app/src/Application.kt
+++ app/src/Application.kt
@@ -1,15 +1,35 @@
 package lerpmusic.website
 
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.autohead.*
-import io.ktor.server.plugins.callid.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.partialcontent.*
-import lerpmusic.website.config.configureLogging
-import lerpmusic.website.config.configureRouting
-import lerpmusic.website.config.configureWebSockets
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.http.content.resolveResource
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.plugins.autohead.AutoHeadResponse
+import io.ktor.server.plugins.callid.CallId
+import io.ktor.server.plugins.callid.callIdMdc
+import io.ktor.server.plugins.callid.generate
+import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.partialcontent.PartialContent
+import io.ktor.server.request.httpMethod
+import io.ktor.server.request.uri
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondRedirect
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import io.ktor.server.websocket.WebSockets
+import io.ktor.server.websocket.pingPeriod
+import io.ktor.server.websocket.timeout
+import kotlinx.serialization.json.Json
+import lerpmusic.website.consensus.consensusSessionRoutes
+import lerpmusic.website.masterportfolio.masterPortfolioRoutes
+import lerpmusic.website.portfolio.portfolioRoutes
+import mu.KotlinLogging.logger
+import org.slf4j.event.Level
+import kotlin.time.Duration.Companion.seconds
 
 @Suppress("unused")
 fun Application.lerpMusicModule() {
@@ -30,3 +50,49 @@ fun Application.lerpMusicModule() {
     configureWebSockets()
     configureRouting()
 }
+
+fun Application.configureRouting() {
+    routing {
+        staticResources("/static", "static")
+
+        portfolioRoutes()
+        masterPortfolioRoutes()
+        consensusSessionRoutes()
+
+        get("/") {
+            val resource = call.resolveResource("/static/index.html")!!
+            call.respond(resource)
+        }
+
+        get("/index.html") {
+            call.respondRedirect("/")
+        }
+    }
+}
+
+fun Application.configureLogging() {
+    install(CallLogging) {
+        level = Level.INFO
+
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val uri = call.request.uri
+            "Status: $status, HTTP method: $httpMethod, URI: $uri"
+        }
+
+        callIdMdc("call-id")
+    }
+}
+
+fun Application.configureWebSockets() {
+    install(WebSockets) {
+        pingPeriod = 15.seconds
+        timeout = 15.seconds
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+        contentConverter = KotlinxWebsocketSerializationConverter(Json)
+    }
+}
+
+private val logger = logger {}
